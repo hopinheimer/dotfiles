0a2ae99c26de0f12229055846e08de5e1f339e4e
Hopinheimer <knmanas6@gmail.com>
2025-01-19 21:38:19 -0500

    init

---
+++ nvim/lua/hopinheimer/init.lua
@@ -0,0 +1,6 @@
+-- import utils 
+require("hopinheimer.remap")
+require("hopinheimer.lazy_init")
+require("hopinheimer.set")
+
+
---
+++ nvim/lua/hopinheimer/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "Up", "<C-n>")
+vim.keymap.set("n", "Down", "<C-m>")
+vim.keymap.set("n", "Left", "<C-,>")
+vim.keymap.set("n", "Right", "<C-.>")
+
+vim.keymap.set("n", "<leader>e", "#")
